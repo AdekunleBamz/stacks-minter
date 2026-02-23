a84ddff3ba4b74ce6d5f788ecbb7239ebfd16a9d
Adekunle Bamzz <bams.kunle@gmail.com>
2026-02-23 16:12:51 +0100

    feat: add NFT loading skeleton

---
+++ src/components/NFTLoading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NFTLoading = () => {
+  return (
+    <div className="animate-pulse">
+      <div className="bg-gray-800 h-48 rounded-lg mb-4"></div>
+      <div className="bg-gray-800 h-4 w-3/4 rounded mb-2"></div>
+      <div className="bg-gray-800 h-4 w-1/2 rounded"></div>
+    </div>
+  )
+}
+
+export default NFTLoading
