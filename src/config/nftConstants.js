ad6aea51c73f91090824505bc007a5c8721bb163
Adekunle Bamzz <bams.kunle@gmail.com>
2026-02-23 14:59:50 +0100

    feat: add NFT constants config

---
+++ src/config/nftConstants.js
@@ -0,0 +1,14 @@
+export const NFT_CONTRACTS = {
+  mainnet: {
+    address: 'SP...',
+    name: 'nft-contract'
+  },
+  testnet: {
+    address: 'ST...',
+    name: 'nft-contract'
+  }
+}
+
+export const MINT_FEE = 1 // STX
+export const MAX_SUPPLY = 10000
+export const ROYALTY_PERCENT = 5
